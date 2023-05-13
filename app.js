d4bafe77a50b10fc05e48db6b930839ee449ad5e
FranciscoGontijo <franciscoacmg@gmail.com>
2023-05-13 10:22:00 +1000

    POST Create new user request

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 //Stats model use:
 const statsModel = require('./models/statsModel');
 
+//POST Create new user when signing in: (working)
+app.post('/createnewuser', async (req, res) => {
+    const userEmail = req.body.userEmail;
+    const stats = {
+        userEmail: userEmail,
+        dateStats: [],
+        labelStats: [],
+        labelList: []
+    };
+    try {
+        const newUserStats = await statsModel.create(stats);
+        res.json(newUserStats);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
